3a74204a6812fc886817c3e5e457597cdce8a5e9
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-23 16:13:40 -0500

    saving changes to navbar

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -24,80 +24,75 @@ const Register = () => {
     )
   }
 
-  return (
-    <>
-
-    <container>
-
-      <div className='justify-center ml-10 mr-10 '>
-        <div className='justify-center col-lg-7 py-3 py-md-5'>
-          <h1 className='text-center font-serif text-red-600 mt-10 text-bold'>
-            Register
-          </h1>
-          <br />
-          <p className='font-serif container mx-auto whitespace-normal flex-auto center-alig mr-20 text-lg'>
-            Once registered, you will be able to search for episodes of your
-            favorite podcast, save episodes to listen to in the future and keep
-            track of episodes you have already heard.  Lastly, you are able to hear the podcast from within the app itself.
-          </p>
-        </div>
+  return <>
 
 
+
+    <div>
+      <div>
+        <h1 className={'font-sans text-red-600 mt-10 text-bold'}>
+          Create an Account
+        </h1>
+        <br />
+        <p className='font-sans container mx-auto whitespace-normal flex-auto center-alig mr-20 text-lg'>
+          Once registered, you will be able to search for episodes of your
+          favorite podcast, save episodes to listen to in the future and keep
+          track of episodes you have already heard.  Lastly, you are able to hear the podcast from within the app itself.
+        </p>
+      </div>
+     
+        <div className='static font-sans text-slate-500' onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <input
+              onChange={e => setFullName(e.target.value)}
+              id='fullName-register'
+              name='fullName'
+              className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
+              type='text'
+              placeholder='Enter your Name'
+              autoComplete='off'
+            />
         
-        <div className='static font-serif  text-slate-500'>
-          <form onSubmit={handleSubmit}>
-            <div className='form-group'>
-              <input
-                onChange={e => setFullName(e.target.value)}
-                id='fullName-register'
-                name='fullName'
-                className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
-                type='text'
-                placeholder='Enter your Name'
-                autoComplete='off'
-              />
-            </div>
-            <br />
-            <div className='form-group'>
-              {''}
-              <input
-                onChange={e => setEmail(e.target.value)}
-                id='email-register'
-                name='email'
-                className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
-                type='text'
-                placeholder='Enter your email '
-                autoComplete='off'
-              />
-            </div>
-            <br />
-            <div className='form-group'>
-       
-       
-              <input
-                onChange={e => setPassword(e.target.value)}
-                id='password-register'
-                name='password'
-                className='border-red-700 w-100 h-19 text-lg file:form-control'
-                type='password'
-                placeholder='Create a password'
-              />
-            </div>
-            <button
-              type='submit'
-              className='font-serif py-20 mt-4 btn btn-md btn-danger btn-block'
-            >
-              Submit
-            </button>
-          </form>
+          <br />
+          <div className='form-group'>
+            {''}
+            <input
+              onChange={e => setEmail(e.target.value)}
+              id='email-register'
+              name='email'
+              className='border-1 border-red-600 w-100 h-19 text-lg file:form-control'
+              type='text'
+              placeholder='Enter your email '
+              autoComplete='off'
+            />
+          </div>
+          <br />
+          <div className='form-group'>
+     
+     
+            <input
+              onChange={e => setPassword(e.target.value)}
+              id='password-register'
+              name='password'
+              className='border-red-700 w-100 h-19 text-lg file:form-control'
+              type='password'
+              placeholder='Create a password'
+            />
+          </div>
+          <button
+            type='submit'
+            className='font-serif py-20 mt-4 btn btn-md btn-danger btn-block'
+          >
+            Submit
+          </button>
         </div>
       </div>
+    </div>
 
-      </container>
+ 
 
 
-    </>
-  )
+  </>;
 }
 
 export default Register
--- src/components/layouts/Menu.jsx
+++ src/components/layouts/Menu.jsx
@@ -3,64 +3,46 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
-function Menu () {
-  //if localstorage token returns, then you may access search, following, listened, logout
-  //if not, you can see register and login
+// function Menu () {
+//   //if localstorage token returns, then you may access search, following, listened, logout
+//   //if not, you can see register and login
 
-  //log in/log out requires refresh - solve second
-  // const [navSnippet, setNavSnippet] = useState(null)
-  const [token, setToken] = useState(localStorage.token)
+//   //log in/log out requires refresh - solve second
+//   // const [navSnippet, setNavSnippet] = useState(null)
+//   const [token, setToken] = useState(localStorage.token)
 
  
-  useEffect(() => {
+//   useEffect(() => {
     
-    console.log();
-  }, [token])
-  
-  
+//     console.log();
+//   }, [token])
+
+
+function Menu () {
   return (
-
-    <Navbar className='text-white background-color: bg-red-600 ' variant='light'>
-
-      <Container className="text-white text-9xl">
-        <Navbar.Brand className='font-serif text-white text-9xl' href='/'>
-          Your Podcasts
-        </Navbar.Brand>
-
-        <Nav className=' text-red-700 justify-center bold font-serif antialiased  background-color:(246,0,1) text-2xl'>
-
-          <div className="bold text-white place-self-start">
-          {/* <Nav.Link className='text-bold place-self-start' href='/'>YourPodcasts | </Nav.Link> */}
-          {/* {loggedin}{loggedout} */}
-          {/* {navSnippet} */}
-          
-          {token ? 
-          
-          <>
-          <Nav.Link className="text-white" href='/searchpodcast'>Search </Nav.Link><Nav.Link className="text-white" href='/following'>Following </Nav.Link>
-        <Nav.Link className="text-white" href='/listened'>Listened</Nav.Link>
-        <Nav.Link className="text-white" href='/logout'>Quit</Nav.Link>
-        </>
-          
-          
-          :
-          <> 
-          
-            <Nav.Link className="text-white" href='/register'>Register</Nav.Link><Nav.Link className="text-white" href='/login'>Login</Nav.Link><Nav.Link className="text-white" href='/searchpodcast'>Search</Nav.Link>
-          
-          </>
-          
-        
-       
-        }
-          
-          </div>
-        </Nav>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className='bg-red-600' >
+        <Container>
+          <Navbar.Brand className='text-white font-sans' href='/'>You Are What You Listen To</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link className='text-sm text-white font-sans' href='/listened'>Podcasts You've Listened To</Nav.Link>
+            <Nav.Link className='text-sm text-white font-sans' href='/following'>Podcasts You're Following</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className='text-white text-2xl font-sans' href='/searchpodcast'>Search</Nav.Link>
+            <Nav.Link className='text-white text-2xl font-sans' href='/register'>Create Account</Nav.Link>
+            <Nav.Link className='text-white text-2xl font-sans' href='/login'>Login</Nav.Link>
+            <Nav.Link className='text-white text-2xl font-sans'  href='/logout'>Logout</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
 export default Menu
 
 
+          
+       
+          
